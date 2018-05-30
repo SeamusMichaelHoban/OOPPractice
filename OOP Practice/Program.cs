b88c95977324b7d449715683c92ecf6260954edc
SeamusMichaelHoban <seamushoban2000@gmail.com>
2018-05-30 08:42:56 -0400

    added Dog

---
+++ OOP Practice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog woofie = new Dog(15, 120, 900, 0);
+            woofie.Run();
+            Dog spot = new Dog();
+            spot.RunningSpeed = 25;
+            spot.Run();
+            woofie.RunningSpeed = 100;
+            woofie.Run();
+            spot.Run();
+
+        }
+    }
+}
